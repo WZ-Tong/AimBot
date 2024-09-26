ada1b19d025de1ae51f563dddd5312600e17357e
WZTong <wztong@vip.qq.com>
2024-09-26 15:41:17 +0800

    Change to verilog repr (FK PGS)

--- source/util/iic_tx.sv
+++ source/util/iic_tx.sv
@@ -20,23 +20,18 @@ module iic_tx #(
 localparam WRITE = 1'b0;
 localparam ACK   = 1'b1;
 
-typedef enum logic [3:0] {
-	IDLE,
-	START,
+localparam IDLE              = 4'b0000;
+localparam START             = 4'b0001;
+localparam DEVICE_ADDR       = 4'b0010;
+localparam RW                = 4'b0011;
+localparam DEVICE_ADDR_ACK   = 4'b0100;
+localparam REGISTER_ADDR     = 4'b0101;
+localparam REGISTER_ADDR_ACK = 4'b0110;
+localparam WRITE_DATA        = 4'b0111;
+localparam WRITE_DATA_ACK    = 4'b1000;
+localparam STOP              = 4'b1001;
 
-	DEVICE_ADDR,
-	RW,
-	DEVICE_ADDR_ACK,
-
-	REGISTER_ADDR,
-	REGISTER_ADDR_ACK,
-
-	WRITE_DATA,
-	WRITE_DATA_ACK,
-
-	STOP
-} state_t;
-state_t state;
+reg [3:0] state;
 assign busy = state != IDLE;
 
 reg sda_en, sda_data;
@@ -45,7 +40,12 @@ assign sda = sda_en ? sda_data : 1'bz;
 localparam MAX_LEN = DEVICE_ADDR_LEN > REG_ADDR_LEN ? (DEVICE_ADDR_LEN > DATA_LEN ? DEVICE_ADDR_LEN : DATA_LEN) : (REG_ADDR_LEN > DATA_LEN ? REG_ADDR_LEN : DATA_LEN);
 reg [$clog2(MAX_LEN)-1:0] data_count;
 
-localparam CLK_HALF = SCL_CLK_DIV / 2;
+localparam CLK_START  = 0                  ;
+localparam CLK_HALF   = SCL_CLK_DIV / 2    ;
+localparam CLK_BEGIN  = SCL_CLK_DIV / 4    ;
+localparam CLK_END    = SCL_CLK_DIV * 3 / 4;
+localparam CLK_FINISH = SCL_CLK_DIV - 1    ;
+
 reg [$clog2(SCL_CLK_DIV)-1:0] clk_count;
 
 always_ff @(posedge clk or negedge rstn) begin
@@ -59,6 +59,7 @@ always_ff @(posedge clk or negedge rstn) begin
 	end else begin
 		case (state)
 			IDLE : begin
+				scl <= #1 1'b1;
 				if (start) begin
 					state     <= #1 START;
 					clk_count <= #1 'b0;
@@ -69,21 +70,47 @@ always_ff @(posedge clk or negedge rstn) begin
 					sda_en    <= #1 'b1;
 					sda_data  <= #1 'b0;
 					clk_count <= #1 clk_count + 1'b1;
-				end else if (clk_count == SCL_CLK_DIV - 1) begin
-					state     <= #1 DEVICE_ADDR;
-					clk_count <= #1 'b0;
+				end else if (clk_count == CLK_FINISH) begin
+					state      <= #1 DEVICE_ADDR;
+					scl        <= #1 'b0;
+					data_count <= #1 DEVICE_ADDR_LEN - 1;
+					clk_count  <= #1 'b0;
 				end else begin
 					clk_count <= #1 clk_count + 1'b1;
 				end
 			end
 			DEVICE_ADDR : begin
-
+				clk_count <= #1 clk_count + 1'b1;
+				if (clk_count == CLK_START) begin
+					sda_data <= #1 device_addr[data_count];
+				end else if (clk_count == CLK_BEGIN) begin
+					scl <= #1 'b1;
+				end else if (clk_count == CLK_END) begin
+					scl <= #1 'b0;
+				end else if (clk_count == CLK_FINISH) begin
+					if (data_count != 0) begin
+						data_count <= #1 data_count - 1'b1;
+					end else begin
+						state <= #1 RW;
+					end
+					clk_count <= #1 'b0;
+				end
 			end
 			RW : begin
-
+				clk_count <= #1 clk_count + 1'b1;
+				if (clk_count == CLK_START) begin
+					sda_data <= #1 WRITE;
+				end else if (clk_count == CLK_BEGIN) begin
+					scl <= #1 'b1;
+				end else if (clk_count == CLK_END) begin
+					scl <= #1 'b0;
+				end else if (clk_count == CLK_FINISH) begin
+					sda_en <= #1 'b0;
+					state  <= #1 DEVICE_ADDR_ACK;
+				end
 			end
 			DEVICE_ADDR_ACK : begin
-
+				clk_count <= #1 clk_count + 1'b1;
 			end
 			REGISTER_ADDR : begin
 
