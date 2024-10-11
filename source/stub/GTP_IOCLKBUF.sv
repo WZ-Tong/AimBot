141bdac2189d7442243c9523508ff9c76d00f74b
WZTong <wztong@vip.qq.com>
2024-10-11 15:47:36 +0800

    Add GTP stub

---
+++ source/stub/GTP_IOCLKBUF.sv
@@ -0,0 +1,7 @@
+module GTP_IOCLKBUF #(parameter GATE_EN = "FALSE") (
+    output CLKOUT,
+    input  CLKIN ,
+    input  DI
+);
+
+endmodule : GTP_IOCLKBUF
---
+++ source/stub/GTP_IOCLKDIV.sv
@@ -0,0 +1,10 @@
+module GTP_IOCLKDIV #(
+    parameter GRS_EN     = "TRUE",
+    parameter DIV_FACTOR = "2"
+) (
+    output CLKDIVOUT,
+    input  CLKIN    ,
+    input  RST_N
+);
+
+endmodule : GTP_IOCLKDIV
