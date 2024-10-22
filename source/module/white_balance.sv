626dc8404dcea4be544057f5e94cf118298edc77
WZTong <wztong@vip.qq.com>
2024-10-22 21:03:30 +0800

    Mark all debug

--- source/module/white_balance.sv
+++ source/module/white_balance.sv
@@ -6,16 +6,16 @@ module white_balance #(
     output [49:0] o_pack
 );
 
-    wire                     clk    ;
-    wire                     i_vsync;
-    wire                     i_hsync;
-    wire                     i_href ;
-    wire                     i_de   ;
-    wire [              7:0] i_r    ;
-    wire [              7:0] i_g    ;
-    wire [              7:0] i_b    ;
-    wire [$clog2(H_ACT)-1:0] i_x    ;
-    wire [$clog2(V_ACT)-1:0] i_y    ;
+    wire clk    ;
+    wire i_vsync;
+    wire i_hsync;
+    wire i_href ;
+    wire i_de   ;
+    wire [              7:0] i_r    /*synthesis PAP_MARK_DEBUG="true"*/;
+    wire [              7:0] i_g    /*synthesis PAP_MARK_DEBUG="true"*/;
+    wire [              7:0] i_b    /*synthesis PAP_MARK_DEBUG="true"*/;
+    wire [$clog2(H_ACT)-1:0] i_x;
+    wire [$clog2(V_ACT)-1:0] i_y;
 
     hdmi_unpack u_hdmi_unpack (
         .pack (i_pack ),
@@ -31,16 +31,16 @@ module white_balance #(
         .y    (i_y    )
     );
 
-    localparam FRAME_TOTAL = V_ACT*H_ACT;
-    localparam TRIM_BITS = $clog2(FRAME_TOTAL);
+    localparam FRAME_TOTAL = V_ACT*H_ACT        ;
+    localparam TRIM_BITS   = $clog2(FRAME_TOTAL);
 
     reg [$clog2(FRAME_TOTAL*256)-1:0] r_last_sum, r_current_sum /*synthesis PAP_MARK_DEBUG="true"*/;
     reg [$clog2(FRAME_TOTAL*256)-1:0] g_last_sum, g_current_sum /*synthesis PAP_MARK_DEBUG="true"*/;
     reg [$clog2(FRAME_TOTAL*256)-1:0] b_last_sum, b_current_sum /*synthesis PAP_MARK_DEBUG="true"*/;
 
-    wire [$clog2(256)-1:0] r_v;
-    wire [$clog2(256)-1:0] g_v;
-    wire [$clog2(256)-1:0] b_v;
+    wire [7:0] r_v /*synthesis PAP_MARK_DEBUG="true"*/;
+    wire [7:0] g_v /*synthesis PAP_MARK_DEBUG="true"*/;
+    wire [7:0] b_v /*synthesis PAP_MARK_DEBUG="true"*/;
 
     assign r_v = r_last_sum[$clog2(FRAME_TOTAL*256)-1:TRIM_BITS];
     assign g_v = g_last_sum[$clog2(FRAME_TOTAL*256)-1:TRIM_BITS];
@@ -49,8 +49,8 @@ module white_balance #(
     wire [$clog2(256*3)-1:0] s_v;
     assign s_v = r_v + g_v + b_v;
 
-    reg  [$clog2(256)-1:0] k_v  ;
-    wire [$clog2(256)-1:0] k_v_w;
+    reg  [7:0] k_v   /*synthesis PAP_MARK_DEBUG="true"*/;
+    wire [7:0] k_v_w;
     assign k_v_w = s_v / 3;
     always_ff @(posedge clk) begin
         k_v <= #1 k_v_w;
@@ -95,12 +95,12 @@ module white_balance #(
     mul_32_16 u_mul_g (.clk(clk), .a(rev_g_v), .b(g_kv), .p(g_new_full));
     mul_32_16 u_mul_b (.clk(clk), .a(rev_b_v), .b(b_kv), .p(b_new_full));
 
-    wire [15:0] r_new, g_new, b_new;
+    wire [15:0] r_new, g_new, b_new /*synthesis PAP_MARK_DEBUG="true"*/;
     assign r_new = r_new_full[47:32];
     assign g_new = g_new_full[47:32];
     assign b_new = b_new_full[47:32];
 
-    reg [7:0] r_r, r_g, r_b;
+    reg [7:0] r_r, r_g, r_b /*synthesis PAP_MARK_DEBUG="true"*/;
     always_ff @(posedge clk) begin
         r_r <= #1 r_new>=16'h00FF ? 8'hFF : r_new[7:0];
         r_g <= #1 g_new>=16'h00FF ? 8'hFF : g_new[7:0];
