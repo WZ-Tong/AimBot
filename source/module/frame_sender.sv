d1a6623b8925b121105303d9e1f0ad83b907bf81
WZTong <wztong@vip.qq.com>
2024-10-23 21:00:27 +0800

    Fix instantize

--- source/module/frame_sender.sv
+++ source/module/frame_sender.sv
@@ -72,50 +72,6 @@ module frame_sender #(
         .almost_empty(/*unused*/)
     );
 
-
-    wire        rstn               ;
-    wire        app_data_in_valid  ;
-    wire [ 7:0] app_data_in        ;
-    wire [15:0] app_data_length    ;
-    wire        app_data_request   ;
-    wire        udp_send_ack       ;
-    wire        arp_req            ;
-    wire        arp_found          ;
-    wire        mac_not_exist      ;
-    wire        mac_send_end       ;
-    wire [ 7:0] udp_rec_rdata      ;
-    wire [15:0] udp_rec_data_length;
-    wire        udp_rec_data_valid ;
-    wire        mac_data_valid     ;
-    wire [ 7:0] mac_tx_data        ;
-    wire        rx_en              ;
-    wire [ 7:0] mac_rx_datain      ;
-    udp_ip_mac_top #(
-        .LOCAL_MAC (LOCAL_MAC ),
-        .LOCAL_IP  (LOCAL_IP  ),
-        .LOCAL_PORT(LOCAL_PORT),
-        .DEST_IP   (DEST_IP   ),
-        .DEST_PORT (DEST_PORT )
-    ) u_udp_ip_mac_top (
-        .rgmii_clk          (rgmii_clk          ),
-        .rstn               (rstn               ),
-        .app_data_in_valid  (app_data_in_valid  ),
-        .app_data_in        (app_data_in        ),
-        .app_data_length    (app_data_length    ),
-        .app_data_request   (app_data_request   ),
-        .udp_send_ack       (udp_send_ack       ),
-        .arp_req            (arp_req            ),
-        .arp_found          (arp_found          ),
-        .mac_not_exist      (mac_not_exist      ),
-        .mac_send_end       (mac_send_end       ),
-        .udp_rec_rdata      (udp_rec_rdata      ),
-        .udp_rec_data_length(udp_rec_data_length),
-        .udp_rec_data_valid (udp_rec_data_valid ),
-        .mac_data_valid     (mac_data_valid     ),
-        .mac_tx_data        (mac_tx_data        ),
-        .rx_en              (rx_en              ),
-        .mac_rx_datain      (mac_rx_datain      )
-    );
-
+    // TODO: Place `udp_sender` here
 
 endmodule : frame_sender
