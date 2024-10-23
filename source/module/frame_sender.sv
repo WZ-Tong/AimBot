3893e1914e57a8b7f1c80ce1e5fbf1794283e50b
WZTong <wztong@vip.qq.com>
2024-10-23 15:47:34 +0800

    Fix signals

--- source/module/frame_sender.sv
+++ source/module/frame_sender.sv
@@ -11,19 +11,22 @@ module frame_sender (
     output [ 3:0] rgmii_txd
 );
 
-    wire cam_clk, rgmii_clk;
+    wire cam_clk  ;
+    wire rgmii_clk;
 
     wire href ;
     wire hsync;
     wire vsync;
-    wire de   ;
 
     wire [7:0] r;
     wire [7:0] g;
     wire [7:0] b;
 
-    wire [10:0] x;
-    wire [ 9:0] y;
+    wire       tx_valid;
+    wire [7:0] tx_data ;
+    wire       rx_error;
+    wire       rx_valid;
+    wire [7:0] rx_data ;
 
     hdmi_unpack u_hdmi_unpack (
         .pack (i_pack ),
@@ -31,19 +34,10 @@ module frame_sender (
         .href (href   ),
         .hsync(hsync  ),
         .vsync(vsync  ),
-        .de   (de     ),
         .r    (r      ),
         .g    (g      ),
-        .b    (b      ),
-        .x    (x      ),
-        .y    (y      )
+        .b    (b      )
     );
-
-    wire       tx_valid;
-    wire [7:0] tx_data ;
-    wire       rx_error;
-    wire       rx_valid;
-    wire [7:0] rx_data ;
 
     rgmii u_rgmii (
         .rgmii_clk   (rgmii_clk   ),
