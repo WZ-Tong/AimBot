f931c7a96888f99507f33e3eec1496e7be6d394c
WZTong <wztong@vip.qq.com>
2024-10-26 17:07:46 +0800

    Impl swap read (not tested)

--- source/module/frame_buffer.sv
+++ source/module/frame_buffer.sv
@@ -1,20 +1,24 @@
-module frame_buffer (
-    input         rstn     ,
-    input  [48:0] cam1_pack,
-    input  [48:0] cam2_pack,
-    input         trig     ,
+module frame_buffer #(
+    parameter H_ACT = 1280,
+    parameter V_ACT = 720
+) (
+    input             rstn     ,
+    input      [48:0] cam1_pack,
+    input      [48:0] cam2_pack,
+    input             trig     ,
 
-    input         rclk     ,
-    input         read_en  ,
-    output        cam_id   ,
-    output [15:0] cam1_data,
-    output [15:0] cam2_data,
+    input             rclk     ,
+    input             read_en  ,
+    output reg        cam_id   ,
+    output reg        valid    ,
+    output reg [15:0] cam_data ,
 
-    output        error
+    output            error
 );
 
-    // FIFO control
     reg wr_rstn;
+    reg cam1_re;
+    reg cam2_re;
 
     wire        cam1_clk  ;
     wire        cam1_vsync;
@@ -32,23 +36,23 @@ module frame_buffer (
         .b    (cam1_b    )
     );
 
-    wire [15:0] cam1_data;
-    assign cam1_data = {cam1_r[7:3], cam1_g[7:2], cam1_b[7:3]};
+    wire [15:0] cam1_wdata, cam1_rdata;
+    assign cam1_wdata = {cam1_r[7:3], cam1_g[7:2], cam1_b[7:3]};
 
-    wire cam1_re, cam1_ready, cam1_full;
-    async_fifo u_cam1_buffer (
+    wire cam1_ready, cam1_full;
+    async_fifo_16_1280 u_cam1_buffer (
         // Write
         .wr_clk      (cam1_clk  ),
         .wr_rst      (~wr_rstn  ),
         .wr_en       (cam1_de   ),
-        .wr_data     (cam1_data ),
+        .wr_data     (cam1_wdata),
         .wr_full     (cam1_full ),
         .almost_full (cam1_ready),
         // Read
         .rd_clk      (rclk      ),
         .rd_rst      (~wr_rstn  ),
         .rd_en       (cam1_re   ),
-        .rd_data     (cam1_data ),
+        .rd_data     (cam1_rdata),
         .rd_empty    (/*unused*/),
         .almost_empty(/*unused*/)
     );
@@ -76,23 +80,23 @@ module frame_buffer (
         .b    (cam2_b    )
     );
 
-    wire [15:0] cam2_data ;
+    wire [15:0] cam2_wdata, cam2_rdata;
     assign cam2_data = {cam2_r[7:3], cam2_g[7:2], cam2_b[7:3]};
 
-    wire cam2_re, cam2_ready, cam2_full;
-    async_fifo u_cam2_buffer (
+    wire cam2_ready, cam2_full;
+    async_fifo_16_1280 u_cam2_buffer (
         // Write
         .wr_clk      (cam2_clk  ),
         .wr_rst      (~wr_rstn  ),
         .wr_en       (cam2_de   ),
-        .wr_data     (cam2_data ),
+        .wr_data     (cam2_wdata),
         .wr_full     (cam2_full ),
         .almost_full (cam2_ready),
         // Read
         .rd_clk      (rclk      ),
         .rd_rst      (~wr_rstn  ),
         .rd_en       (cam2_re   ),
-        .rd_data     (cam2_data ),
+        .rd_data     (cam2_rdata),
         .rd_empty    (/*unused*/),
         .almost_empty(/*unused*/)
     );
@@ -104,17 +108,17 @@ module frame_buffer (
         .o_rst(cam2_error)
     );
 
-    localparam IDLE       = 2'b00;
-    localparam WAIT_VSYNC = 2'b01;
-    localparam READ_CAM1  = 2'b10;
-    localparam READ_CAM2  = 2'b11;
+    localparam IDLE       = 3'b000;
+    localparam WAIT_VSYNC = 3'b001;
+    localparam WAIT_CAM1  = 3'b010;
+    localparam READ_CAM1  = 3'b011;
+    localparam WAIT_CAM2  = 3'b100;
+    localparam READ_CAM2  = 3'b101;
 
-    reg [1:0] state;
-    assign cam_id  = state==READ_CAM2 ? 1'b1 : 1'b0;
-    assign cam1_re = state==READ_CAM1 && read_en;
-    assign cam2_re = state==READ_CAM2 && read_en;
+    reg [2:0] state;
 
-    reg [9:0] y;
+    reg [$clog2(H_ACT):0] x;
+    reg [$clog2(V_ACT):0] y;
 
     reg cvs_err, cam1_vsync_d, cam2_vsync_d;
 
@@ -126,11 +130,17 @@ module frame_buffer (
         if(~rstn) begin
             cvs_err <= #1 'b0;
             state   <= #1 IDLE;
+            cam1_re <= #1 'b0;
+            cam2_re <= #1 'b0;
+            cam_id  <= #1 'b0;
         end else begin
             case (state)
                 IDLE : begin
+                    cam_id  <= #1 'b0;
                     cvs_err <= #1 'b0;
                     wr_rstn <= #1 'b0;
+                    cam1_re <= #1 'b0;
+                    cam2_re <= #1 'b0;
                     if (trig) begin
                         state        <= #1 WAIT_VSYNC;
                         cam1_vsync_d <= #1 'b0;
@@ -138,6 +148,9 @@ module frame_buffer (
                     end
                 end
                 WAIT_VSYNC : begin
+                    cam_id       <= #1 'b0;
+                    cam1_re      <= #1 'b0;
+                    cam2_re      <= #1 'b0;
                     cam1_vsync_d <= #1 cam1_vsync;
                     cam2_vsync_d <= #1 cam2_vsync;
                     wr_rstn      <= #1 'b1;
@@ -146,14 +159,69 @@ module frame_buffer (
                         cvs_err <= #1 'b1;
                     end else if (cam1_vsync&&cam2_vsync) begin
                         y     <= #1 'b0;
+                        state <= #1 WAIT_CAM1;
+                    end
+                end
+                WAIT_CAM1 : begin
+                    x       <= #1 'b0;
+                    cam_id  <= #1 'b0;
+                    cam2_re <= #1 'b0;
+                    if (cam1_ready) begin
                         state <= #1 READ_CAM1;
                     end
                 end
                 READ_CAM1 : begin
-                    // TODO
+                    cam_id  <= #1 'b0;
+                    cam2_re <= #1 'b0;
+                    if (read_en && x!=H_ACT-1) begin
+                        cam1_re <= #1 'b1;
+                    end else begin
+                        cam1_re <= #1 'b0;
+                    end
+                    if (cam1_re) begin
+                        valid    <= #1 'b1;
+                        cam_data <= #1 cam1_rdata;
+                        if (x==H_ACT-1) begin
+                            state <= #1 WAIT_CAM2;
+                        end else begin
+                            x <= #1 x + 1'b1;
+                        end
+                    end else begin
+                        valid <= #1 'b0;
+                    end
+                end
+                WAIT_CAM2 : begin
+                    x       <= #1 'b0;
+                    cam_id  <= #1 'b1;
+                    cam1_re <= #1 'b0;
+                    if (cam2_ready) begin
+                        state <= #1 READ_CAM2;
+                    end
                 end
                 READ_CAM2 : begin
-                    // TODO
+                    cam_id  <= #1 'b1;
+                    cam1_re <= #1 'b0;
+                    if (read_en && x!=H_ACT-1) begin
+                        cam2_re <= #1 'b1;
+                    end else begin
+                        cam2_re <= #1 'b0;
+                    end
+                    if (cam2_re) begin
+                        valid    <= #1 'b1;
+                        cam_data <= #1 cam2_rdata;
+                        if (x==H_ACT-1) begin
+                            if (y==V_ACT-1) begin
+                                state <= #1 IDLE;
+                            end else begin
+                                y     <= #1 y + 1'b1;
+                                state <= #1 WAIT_CAM1;
+                            end
+                        end else begin
+                            x <= #1 x + 1'b1;
+                        end
+                    end else begin
+                        valid <= #1 'b0;
+                    end
                 end
                 default : begin
                     state <= #1 IDLE;
