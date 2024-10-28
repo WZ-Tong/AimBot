ed2f6dfa401f5e7be0d927fa7b17e7cd701f8409
WZTong <wztong@vip.qq.com>
2024-10-28 18:47:48 +0800

    Interactive udp sender

--- source/util/send_udp.py
+++ source/util/send_udp.py
@@ -1,6 +1,12 @@
 import socket
+import sys
+
+udp_msg = "A" * 16
+if len(sys.argv) != 0:
+    udp_msg = sys.argv[1]
+    print(udp_msg)
 
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp_addr = ('192.168.2.101', 8080)
-udp_socket.sendto("A".encode('utf-8'), udp_addr)
+udp_socket.sendto(udp_msg.encode('utf-8'), udp_addr)
 udp_socket.close()
