e89072585c353dd01192b1b070a4d71306be95a6
WZTong <wztong@vip.qq.com>
2024-10-28 20:43:12 +0800

    Change mem repr into array repr

--- source/module/udp_reader.sv
+++ source/module/udp_reader.sv
@@ -5,25 +5,25 @@ module udp_reader #(parameter CAPACITY = 1) (
     input                       valid ,
     input      [           7:0] i_data,
 
-    output     [CAPACITY*8-1:0] o_data,
+    output reg [CAPACITY*8-1:0] o_data,
     output reg                  error
 );
 
-    reg [$clog2(CAPACITY)-1:0] wptr          ;
-    reg [                 7:0] mem [CAPACITY];
+    reg [$clog2(CAPACITY)-1:0] wptr;
 
     integer i;
+
     always_ff @(posedge clk or negedge rstn) begin
         if(~rstn) begin
-            error <= #1 'b0;
-            wptr  <= #1 'b0;
-            for (i = 0; i < CAPACITY; i=i+1) begin
-                mem[i] <= #1 'b0;
-            end
+            error  <= #1 'b0;
+            wptr   <= #1 'b0;
+            o_data <= #1 'b0;
         end else if (valid) begin
             if (wptr!=CAPACITY-1) begin
-                mem[wptr] <= #1 i_data;
-                wptr      <= #1 wptr + 1'b1;
+                for (i = 0; i < 8; i=i+1) begin
+                    o_data[wptr*8+i] <= #1 i_data[i];
+                end
+                wptr <= #1 wptr + 1'b1;
             end else begin
                 error <= #1 'b1;
             end
@@ -31,11 +31,6 @@ module udp_reader #(parameter CAPACITY = 1) (
             error <= #1 'b0;
             wptr  <= #1 'b0;
         end
-    end
-
-    genvar j;
-    for (j = 0; j < CAPACITY; j=j+1) begin : gen_unpack
-        assign o_data[(j+1)*8-1:j*8] = mem[j];
     end
 
 endmodule : udp_reader
