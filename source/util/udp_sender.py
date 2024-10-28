b0755c8f15ba299e214cf135c3386be354107c3f
WZTong <wztong@vip.qq.com>
2024-10-28 19:24:44 +0800

    FromHex sender

--- source/util/udp_sender.py
+++ source/util/udp_sender.py
@@ -1,12 +1,13 @@
 import socket
 import sys
 
-udp_msg = "A" * 16
+msg_bytes_720p = 18
+udp_msg = "00" * msg_bytes_720p
 if len(sys.argv) != 0:
     udp_msg = sys.argv[1]
     print(udp_msg)
 
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp_addr = ('192.168.2.101', 8080)
-udp_socket.sendto(udp_msg.encode('utf-8'), udp_addr)
+udp_socket.sendto(bytes.fromhex(udp_msg), udp_addr)
 udp_socket.close()
