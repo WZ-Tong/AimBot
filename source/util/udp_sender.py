97f57095df6a0e11f706ee2b113f48c465e88a27
WZTong <wztong@vip.qq.com>
2024-10-28 21:06:15 +0800

    Auto python

--- source/util/udp_sender.py
+++ source/util/udp_sender.py
@@ -1,13 +1,26 @@
 import socket
 import sys
 
-msg_bytes_720p = 18
-udp_msg = "00" * msg_bytes_720p
-if len(sys.argv) != 0:
-    udp_msg = sys.argv[1]
-    print(udp_msg)
+start_x = sys.argv[1]
+start_y = sys.argv[2]
+end_x = sys.argv[3]
+end_y = sys.argv[4]
+color = sys.argv[5]
+
+start_x = (16*"0" + bin(int(start_x))[2:])[-11:]
+start_y = (16*"0" + bin(int(start_y))[2:])[-10:]
+end_x = (16*"0" + bin(int(end_x))[2:])[-11:]
+end_y = (16*"0" + bin(int(end_y))[2:])[-10:]
+color = (16*"0" + bin(int(color))[2:])[-6:]
+
+udp_msg = "udp_msg = 0b" + start_x + start_y + end_x + end_y + color
+exec(udp_msg)
+udp_msg = (16*"0" + hex(udp_msg)[2:])[-12:]
+print(udp_msg)
+udp_msg = bytes.fromhex(udp_msg)
+print(udp_msg)
 
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp_addr = ('192.168.2.101', 8080)
-udp_socket.sendto(bytes.fromhex(udp_msg), udp_addr)
+udp_socket.sendto(udp_msg, udp_addr)
 udp_socket.close()
