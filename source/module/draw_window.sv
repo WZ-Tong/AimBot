f1c36378ff2d11f3d34b1323cd372c27a677ec6b
WZTong <wztong@vip.qq.com>
2024-10-31 11:20:27 +0800

    Ability to draw center cross (not tested)

--- source/module/draw_window.sv
+++ source/module/draw_window.sv
@@ -23,67 +23,87 @@ module draw_window #(
     localparam H_ACT_BITS = $clog2(H_ACT);
     localparam V_ACT_BITS = $clog2(V_ACT);
 
+    localparam CENTER_WIDTH = BOX_WIDTH * 3;
+
     wire clk;
 
     wire [H_ACT_BITS-1:0] x;
     wire [V_ACT_BITS-1:0] y;
 
-    wire [BOX_NUM-1:0] active;
-    wire [7:0] color_r [BOX_NUM];
-    wire [7:0] color_g [BOX_NUM];
-    wire [7:0] color_b [BOX_NUM];
+    wire [BOX_NUM-1:0] active          ;
+    wire [        7:0] color_r[BOX_NUM];
+    wire [        7:0] color_g[BOX_NUM];
+    wire [        7:0] color_b[BOX_NUM];
 
-    generate
-        genvar i;
-        for (i = 0; i < BOX_NUM; i=i+1) begin : g_boxes
-            wire [H_ACT_BITS-1:0] start_x0, start_x1;
-            assign start_x0 = start_xs[(i+1)*H_ACT_BITS-1:i*H_ACT_BITS];
-            assign start_x1 = start_x0 + BOX_WIDTH;
+    genvar i;
+    for (i = 0; i < BOX_NUM; i=i+1) begin : g_boxes
+        wire [8*3-1:0] color;
+        assign color = colors[(i+1)*(8*3)-1:i*(8*3)];
+        assign {color_r[i], color_g[i], color_b[i]} = color;
 
-            wire [H_ACT_BITS-1:0] end_x0, end_x1;
-            assign end_x0 = end_xs[(i+1)*H_ACT_BITS-1:i*H_ACT_BITS];
-            assign end_x1 = end_x0 + BOX_WIDTH;
+        wire [H_ACT_BITS-1:0] start_x0, start_x1;
+        assign start_x0 = start_xs[(i+1)*H_ACT_BITS-1:i*H_ACT_BITS];
+        assign start_x1 = start_x0 + BOX_WIDTH;
 
-            wire [V_ACT_BITS-1:0] start_y0, start_y1;
-            assign start_y1 = start_ys[(i+1)*V_ACT_BITS-1:i*V_ACT_BITS];
-            assign start_y0 = start_y1 - BOX_WIDTH;
+        wire [H_ACT_BITS-1:0] end_x0, end_x1;
+        assign end_x0 = end_xs[(i+1)*H_ACT_BITS-1:i*H_ACT_BITS];
+        assign end_x1 = end_x0 + BOX_WIDTH;
 
-            wire [V_ACT_BITS-1:0] end_y0, end_y1;
-            assign end_y1 = end_ys[(i+1)*V_ACT_BITS-1:i*V_ACT_BITS];
-            assign end_y0 = end_y1 - BOX_WIDTH;
+        wire [V_ACT_BITS-1:0] start_y0, start_y1;
+        assign start_y1 = start_ys[(i+1)*V_ACT_BITS-1:i*V_ACT_BITS];
+        assign start_y0 = start_y1 - BOX_WIDTH;
 
-            wire [H_ACT_BITS:0] x_total;
-            wire [V_ACT_BITS:0] y_total;
-            assign x_total = start_x0 + end_x0;
-            assign y_total = start_y1 + end_y1;
+        wire [V_ACT_BITS-1:0] end_y0, end_y1;
+        assign end_y1 = end_ys[(i+1)*V_ACT_BITS-1:i*V_ACT_BITS];
+        assign end_y0 = end_y1 - BOX_WIDTH;
 
-            wire [H_ACT_BITS-1:0] x_center;
-            wire [V_ACT_BITS-1:0] y_center;
-            assign x_center = x_total[H_ACT_BITS:1];
-            assign y_center = y_total[V_ACT_BITS:1];
+        // Box is not (0,0)->(0,0)
+        wire start_valid, end_valid, xy_valid;
+        assign start_valid = start_x0!=0 && start_y0!=0;
+        assign end_valid   = end_x0!=0 && end_y0!=0;
+        assign xy_valid    = start_valid||end_valid;
 
-            wire [8*3-1:0] color;
-            assign color = colors[(i+1)*(8*3)-1:i*(8*3)];
-            assign {color_r[i], color_g[i], color_b[i]} = color;
+        // Box is outside inner bound, inside outer bound
+        wire box_outer_active, box_inner_active, box_active;
+        assign box_outer_active = x>=start_x0 && x<=end_x1 && y>=start_y0 && y<=end_y1;
+        assign box_inner_active = x>=start_x1 && x<=end_x0 && y>=start_y1 && y<=end_y0;
+        assign box_active       = box_outer_active && !box_inner_active;
 
-            wire outer_active, inner_active;
-            assign outer_active = x>=start_x0 && x<=end_x1 && y>=start_y0 && y<=end_y1;
-            assign inner_active = x>=start_x1 && x<=end_x0 && y>=start_y1 && y<=end_y0;
+        // Box should be able to contain center cross
+        wire x_gap_valid, y_gap_valid, gap_valid;
+        assign x_gap_valid = (end_x0-start_x0) >= (CENTER_WIDTH*2+BOX_WIDTH);
+        assign y_gap_valid = (end_y1-start_y1) >= (CENTER_WIDTH*2+BOX_WIDTH);
+        assign gap_valid   = x_gap_valid && y_gap_valid;
 
-            wire box_valid, start_valid, end_valid;
-            assign start_valid = start_x0!=0 && start_y0!=0;
-            assign end_valid   = end_x0!=0 && end_y0!=0;
-            assign box_valid   = start_valid||end_valid;
+        wire [H_ACT_BITS:0] x_total_start, x_total_end;
+        wire [V_ACT_BITS:0] y_total_start, y_total_end;
+        assign x_total_start = start_x0 + end_x0;
+        assign y_total_start = start_y1 + end_y1;
+        assign x_total_end   = start_x1 + end_x1;
+        assign y_total_end   = start_y0 + end_y0;
 
-            wire window_valid;
-            assign window_valid = box_valid && outer_active && !inner_active;
+        wire [H_ACT_BITS-1:0] x_center_start, x_center_end;
+        wire [V_ACT_BITS-1:0] y_center_start, y_center_end;
+        assign x_center_start = x_total_start[H_ACT_BITS:1];
+        assign y_center_start = y_total_start[V_ACT_BITS:1];
+        assign x_center_end   = x_total_end[H_ACT_BITS:1];
+        assign y_center_end   = y_total_end[V_ACT_BITS:1];
 
-            wire center_valid;
-            // TODO
+        wire [H_ACT_BITS-1:0] x_cb_start, x_cb_end; // Center boundary
+        wire [V_ACT_BITS-1:0] y_cb_start, y_cb_end; // Center boundary
+        assign x_cb_start = x_center_start - CENTER_WIDTH;
+        assign y_cb_start = y_center_start - CENTER_WIDTH;
+        assign x_cb_end   = x_center_end + CENTER_WIDTH;
+        assign y_cb_end   = y_center_end + CENTER_WIDTH;
 
-            assign active[i] = window_valid || center_valid;
-        end
-    endgenerate
+        // Center cross pattern
+        wire ch_active, cv_active, center_active;
+        assign ch_active     = x>=x_cb_start && x<=x_cb_end && y>=y_center_start && y<=y_center_end;
+        assign cv_active     = y>=y_cb_start && y<=y_cb_end && x>=x_center_start && x<=x_center_end;
+        assign center_active = ch_active || cv_active;
+
+        assign active[i] = (xy_valid && gap_valid) && (box_active || ch_active);
+    end
 
     wire [7:0] hdmi_r;
     wire [7:0] hdmi_g;
