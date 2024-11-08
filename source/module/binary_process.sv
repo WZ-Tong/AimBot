8cdb6fd9594e3e66353539e30dbdeeceeb8d86c5
WZTong <wztong@vip.qq.com>
2024-11-08 13:42:15 +0800

    Change size to 5 since usage dropped

--- source/module/binary_process.sv
+++ source/module/binary_process.sv
@@ -2,7 +2,7 @@ module binary_process #(
     parameter  H_ACT     = 12'd1280                         ,
     parameter  V_ACT     = 12'd720                          ,
 
-    parameter  WIN_SIZE  = 4                                ,
+    parameter  WIN_SIZE  = 5                                ,
     localparam PACK_SIZE = 3*8+4+$clog2(H_ACT)+$clog2(V_ACT)
 ) (
     input                  rstn  ,
