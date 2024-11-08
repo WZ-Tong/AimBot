1bfa58e79e6f74331704596f6fce5f8e53f86d23
WZTong <wztong@vip.qq.com>
2024-11-08 13:21:00 +0800

    Cleanup bin buffers

--- source/module/bin_buffers.sv
+++ source/module/bin_buffers.sv
@@ -1,45 +1,69 @@
 module bin_buffers #(
-    parameter CAPACITY = 12'd1280,
-    parameter PARALLEL = 5
+    parameter  H_ACT     = 12'd1280                         ,
+    parameter  V_ACT     = 12'd720                          ,
+    parameter  COLUMN    = 4                                ,
+    localparam PACK_SIZE = 3*8+4+$clog2(H_ACT)+$clog2(V_ACT)
 ) (
-    input                 clk   ,
-    input                 rstn  ,
-    input                 bin   ,
-    input                 de    ,
-    input                 hsync ,
-    input                 cls   ,
-    output [PARALLEL-1:0] window
+    input                  rstn  ,
+    input  [PACK_SIZE-1:0] i_pack,
+    output [PACK_SIZE-1:0] o_pack,
+
+    output [   COLUMN-1:0] window
 );
 
-    reg [$clog2(CAPACITY)-1:0] addr;
-    reg [$clog2(PARALLEL)-1:0] ptr ;
+    wire                     clk  ;
+    wire                     hsync;
+    wire                     vsync;
+    wire                     de   ;
+    wire [              7:0] r    ;
+    wire [              7:0] g    ;
+    wire [              7:0] b    ;
+    wire [$clog2(H_ACT)-1:0] x    ;
+    wire [$clog2(V_ACT)-1:0] y    ;
+    hdmi_unpack #(.H_ACT(H_ACT), .V_ACT(V_ACT)) u_hdmi_unpack (
+        .pack (i_pack),
+        .clk  (clk   ),
+        .hsync(hsync ),
+        .vsync(vsync ),
+        .de   (de    ),
+        .r    (r     ),
+        .g    (g     ),
+        .b    (b     ),
+        .x    (x     ),
+        .y    (y     )
+    );
+    wire bin;
+    assign bin = (r==0&&g==0&&b==0) ? 1'b0 : 1'b1;
+
+    reg [   $clog2(H_ACT)-1:0] addr;
+    reg [$clog2(COLUMN)-1:0] ptr ;
 
     always_ff @(posedge clk or negedge rstn) begin
         if(~rstn) begin
             addr <= #1 'b0;
             ptr  <= #1 'b0;
-        end else if (cls) begin
+        end else if (vsync) begin
             addr <= #1 'b0;
             ptr  <= #1 'b0;
         end else if (hsync) begin
             addr <= #1 'b0;
-            if (ptr!=PARALLEL-1) begin
+            if (ptr!=COLUMN-1) begin
                 ptr <= #1 ptr + 1'b1;
             end else begin
                 ptr <= #1 'b0;
             end
-        end else if (addr!=CAPACITY-1 && de) begin
+        end else if (addr!=H_ACT-1 && de) begin
             addr <= #1 addr + 1'b1;
         end
     end
 
     genvar i;
-    for (i = 0; i < PARALLEL; i=i+1) begin: g_rams
+    for (i = 0; i < COLUMN; i=i+1) begin: g_rams
         reg  read;
         wire wen ;
         assign wen = ptr==i && de;
 
-        reg [CAPACITY-1:0] ram;
+        reg [H_ACT-1:0] ram;
         always_ff @(posedge clk or negedge rstn) begin
             if(~rstn) begin
                 ram  <= #1 'b0;
@@ -65,8 +89,8 @@ module bin_buffers #(
         end
     end
 
-    if (PARALLEL==3) begin: g_temp_link_3
-        reg [CAPACITY-1:0] temp_window;
+    if (COLUMN==3) begin: g_temp_link_3
+        reg [H_ACT-1:0] temp_window;
         always_comb begin
             unique case (ptr)
                 'd0 : begin
@@ -85,8 +109,8 @@ module bin_buffers #(
             temp_window[2] = current;
         end
         assign window = temp_window;
-    end else if (PARALLEL==4) begin: g_temp_link_4
-        reg [CAPACITY-1:0] temp_window;
+    end else if (COLUMN==4) begin: g_temp_link_4
+        reg [H_ACT-1:0] temp_window;
         always_comb begin
             unique case (ptr)
                 'd0 : begin
@@ -113,8 +137,8 @@ module bin_buffers #(
             temp_window[3] = current;
         end
         assign window = temp_window;
-    end else if (PARALLEL==5) begin: g_temp_link_5
-        reg [CAPACITY-1:0] temp_window;
+    end else if (COLUMN==5) begin: g_temp_link_5
+        reg [H_ACT-1:0] temp_window;
         always_comb begin
             unique case (ptr)
                 'd0 : begin
@@ -154,5 +178,44 @@ module bin_buffers #(
     end else begin: g_temp_link_unimpl
         err_mode not_yet_linked();
     end
+
+        localparam DELAY = 1;
+
+    wire o_hsync, o_vsync, o_de;
+    delay #(
+        .DELAY(DELAY),
+        .WIDTH(3    )
+    ) u_sync_de_delay (
+        .clk   (clk                     ),
+        .i_data({hsync, vsync, de}      ),
+        .o_data({o_hsync, o_vsync, o_de})
+    );
+
+    wire [$clog2(H_ACT)-1:0] o_x;
+    wire [$clog2(V_ACT)-1:0] o_y;
+    delay #(
+        .DELAY(DELAY                      ),
+        .WIDTH($clog2(H_ACT)+$clog2(V_ACT))
+    ) u_xy_delay (
+        .clk   (clk       ),
+        .i_data({x, y}    ),
+        .o_data({o_x, o_y})
+    );
+
+    wire [7:0] o_rgb;
+    assign o_rgb = {8{current}};
+
+    hdmi_pack #(.H_ACT(H_ACT), .V_ACT(V_ACT)) u_hdmi_pack (
+        .clk  (clk    ),
+        .hsync(o_hsync),
+        .vsync(o_vsync),
+        .de   (o_de   ),
+        .r    (o_rgb  ),
+        .g    (o_rgb  ),
+        .b    (o_rgb  ),
+        .x    (o_x    ),
+        .y    (o_y    ),
+        .pack (o_pack )
+    );
 
 endmodule : bin_buffers
--- source/module/binary_process.sv
+++ source/module/binary_process.sv
@@ -2,7 +2,7 @@ module binary_process #(
     parameter  H_ACT     = 12'd1280                         ,
     parameter  V_ACT     = 12'd720                          ,
 
-    parameter  WIN_SIZE  = 5                                ,
+    parameter  WIN_SIZE  = 4                                ,
     localparam PACK_SIZE = 3*8+4+$clog2(H_ACT)+$clog2(V_ACT)
 ) (
     input                  rstn  ,
@@ -11,79 +11,16 @@ module binary_process #(
     output [PACK_SIZE-1:0] o_pack
 );
 
-    wire                     clk  ;
-    wire                     hsync;
-    wire                     vsync;
-    wire                     de   ;
-    wire [              7:0] r    ;
-    wire [              7:0] g    ;
-    wire [              7:0] b    ;
-    wire [$clog2(H_ACT)-1:0] x    ;
-    wire [$clog2(V_ACT)-1:0] y    ;
-    hdmi_unpack #(.H_ACT(H_ACT), .V_ACT(V_ACT)) u_hdmi_unpack (
-        .pack (i_pack),
-        .clk  (clk   ),
-        .hsync(hsync ),
-        .vsync(vsync ),
-        .de   (de    ),
-        .r    (r     ),
-        .g    (g     ),
-        .b    (b     ),
-        .x    (x     ),
-        .y    (y     )
-    );
-
-    wire bin;
-    assign bin = (r==0&&g==0&&b==0) ? 1'b0 : 1'b1;
-
     wire [WIN_SIZE-1:0] window;
-    bin_buffers #(.CAPACITY(H_ACT), .PARALLEL(WIN_SIZE)) u_bin_buffers (
-        .clk   (clk   ),
+    bin_buffers #(
+        .H_ACT (H_ACT   ),
+        .V_ACT (V_ACT   ),
+        .COLUMN(WIN_SIZE)
+    ) u_bin_buffers (
         .rstn  (rstn  ),
-        .hsync (hsync ),
-        .cls   (vsync ),
-        .bin   (bin   ),
-        .de    (de    ),
+        .i_pack(i_pack),
+        .o_pack(o_pack),
         .window(window)
-    );
-
-    localparam DELAY = 1;
-
-    wire o_hsync, o_vsync, o_de;
-    delay #(
-        .DELAY(DELAY),
-        .WIDTH(3    )
-    ) u_sync_de_delay (
-        .clk   (clk                     ),
-        .i_data({hsync, vsync, de}      ),
-        .o_data({o_hsync, o_vsync, o_de})
-    );
-
-    wire [$clog2(H_ACT)-1:0] o_x;
-    wire [$clog2(V_ACT)-1:0] o_y;
-    delay #(
-        .DELAY(DELAY                      ),
-        .WIDTH($clog2(H_ACT)+$clog2(V_ACT))
-    ) u_xy_delay (
-        .clk   (clk       ),
-        .i_data({x, y}    ),
-        .o_data({o_x, o_y})
-    );
-
-    wire [7:0] test;
-    assign test = window[0]==0 ? 8'b0 : (~8'b0);
-
-    hdmi_pack #(.H_ACT(H_ACT), .V_ACT(V_ACT)) u_hdmi_pack (
-        .clk  (clk    ),
-        .hsync(o_hsync),
-        .vsync(o_vsync),
-        .de   (o_de   ),
-        .r    (test   ),
-        .g    (test   ),
-        .b    (test   ),
-        .x    (o_x    ),
-        .y    (o_y    ),
-        .pack (o_pack )
     );
 
 endmodule : binary_process
