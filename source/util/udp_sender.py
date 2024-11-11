9f8965da3f46d2450ce9f97a2aa172f0c4ee593a
WZTong <wztong@vip.qq.com>
2024-11-12 01:20:39 +0800

    Auto adjust box count

--- source/util/udp_sender.py
+++ source/util/udp_sender.py
@@ -1,26 +1,32 @@
+# pip install bitarray
+
 import socket
 import sys
+from bitarray import bitarray
 
-start_x = sys.argv[1]
-start_y = sys.argv[2]
-end_x = sys.argv[3]
-end_y = sys.argv[4]
-color = sys.argv[5]
+def gen_udp(start_x, start_y, end_x, end_y, color):
+    start_x = (16*"0" + bin(int(start_x))[2:])[-11:]
+    start_y = (16*"0" + bin(int(start_y))[2:])[-10:]
+    end_x = (16*"0" + bin(int(end_x))[2:])[-11:]
+    end_y = (16*"0" + bin(int(end_y))[2:])[-10:]
+    color = (16*"0" + bin(int(color))[2:])[-6:]
 
-start_x = (16*"0" + bin(int(start_x))[2:])[-11:]
-start_y = (16*"0" + bin(int(start_y))[2:])[-10:]
-end_x = (16*"0" + bin(int(end_x))[2:])[-11:]
-end_y = (16*"0" + bin(int(end_y))[2:])[-10:]
-color = (16*"0" + bin(int(color))[2:])[-6:]
+    udp_msg = start_x + start_y + end_x + end_y + color
+    udp_msg = bytes(bitarray(udp_msg))
+    return udp_msg
 
-udp_msg = "udp_msg = 0b" + start_x + start_y + end_x + end_y + color
-exec(udp_msg)
-udp_msg = (16*"0" + hex(udp_msg)[2:])[-12:]
-print(udp_msg)
-udp_msg = bytes.fromhex(udp_msg)
-print(udp_msg)
-
-udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp_addr = ('192.168.2.101', 8080)
-udp_socket.sendto(udp_msg, udp_addr)
-udp_socket.close()
+if __name__ == '__main__':
+    udp_msg = bytes();
+    for i in range(len(sys.argv)//5):
+        udp_msg = udp_msg + gen_udp(
+            sys.argv[i*5+0+1],
+            sys.argv[i*5+1+1],
+            sys.argv[i*5+2+1],
+            sys.argv[i*5+3+1],
+            sys.argv[i*5+4+1],
+        )
+    print(udp_msg)
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_addr = ('192.168.2.101', 8080)
+    udp_socket.sendto(udp_msg, udp_addr)
+    udp_socket.close()
